9c0d2aaf58a95ffb47ffbd7956247eb5084d34ea
kevin chao <kchao562@gmail.com>
2017-10-12 23:02:47 -0700

    removed keys.js from .gitignore

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  google : {
+    clientID: '393852577124-du4hrae8odanu37sh8cgacmhk3hmolvg.apps.googleusercontent.com',
+    clientSecret: 'n5jR3BfnGWOmsU4U0w-bno5S',
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  }
+}
