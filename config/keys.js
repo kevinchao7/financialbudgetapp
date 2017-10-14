95d52689232790ebebbf5c39dd06ee520740265a
kevin chao <kchao562@gmail.com>
2017-10-14 11:40:15 -0700

    heroku callback link added

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
   google : {
     clientID: '393852577124-du4hrae8odanu37sh8cgacmhk3hmolvg.apps.googleusercontent.com',
     clientSecret: 'n5jR3BfnGWOmsU4U0w-bno5S',
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: "https://financialbudgetapp.herokuapp.com/auth/google/callback"
   }
 }
