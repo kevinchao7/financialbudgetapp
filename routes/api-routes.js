bbabb8b4cf9f668156e1730943840814e06b72f8
kevin chao <kchao562@gmail.com>
2017-10-16 18:48:31 -0700

    Integrated angular iwth api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,7 +9,7 @@ module.exports = (app,db,passport)=>{
   require('./client_route.js')(app,db);
 
   app.get('/auth/google', passport.authenticate('google', { scope : ['profile','email'] } ) );
-  app.get('/auth/google/callback', passport.authenticate('google',{ successRedirect : '/app/dashboard',failureRedirect : '/app/dashboard'} )
+  app.get('/auth/google/callback', passport.authenticate('google',{ successRedirect : '/#!/app/dashboard',failureRedirect : '/#!/app/dashboard'} )
   // ,(req,res)=>{
   //   var userData = req.user.dataValues;
   //   res.render('login',{user : userData});
