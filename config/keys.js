c8408a57e1662d22d78eeee9ce3cd4f5d81dbcea
kevin chao <kchao562@gmail.com>
2017-10-17 06:16:28 -0700

    added jasmine front end changes

--- config/keys.js
+++ config/keys.js
@@ -3,8 +3,8 @@ module.exports = {
   google : {
     clientID: '393852577124-du4hrae8odanu37sh8cgacmhk3hmolvg.apps.googleusercontent.com',
     clientSecret: 'n5jR3BfnGWOmsU4U0w-bno5S',
-    callbackURL: "https://financialbudgetapp.herokuapp.com/auth/google/callback"
-    // callbackURL: "http://localhost:8080/auth/google/callback"
+    // callbackURL: "https://financialbudgetapp.herokuapp.com/auth/google/callback"
+    callbackURL: "http://localhost:8080/auth/google/callback"
   },
   SENDGRID_API_KEY : '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',
   session_key : randomString.generate()
