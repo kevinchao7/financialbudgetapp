32dceaa4ff48fc5ca5f07320d1ef005f096c028e
kevin chao <kchao562@gmail.com>
2017-10-17 16:36:55 -0700

    flex spend controller edit: binding

--- config/keys.js
+++ config/keys.js
@@ -3,9 +3,9 @@ module.exports = {
   google : {
     clientID: '393852577124-du4hrae8odanu37sh8cgacmhk3hmolvg.apps.googleusercontent.com',
     clientSecret: 'n5jR3BfnGWOmsU4U0w-bno5S',
-    callbackURL: "https://financialbudgetapp.herokuapp.com/auth/google/callback"
-    // callbackURL: "http://localhost:8080/auth/google/callback"
+    // callbackURL: "https://financialbudgetapp.herokuapp.com/auth/google/callback"
+    callbackURL: "http://localhost:8080/auth/google/callback"
   },
-  SENDGRID_API_KEY : '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',
+  SENDGRID_API_KEY : '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',
   session_key : randomString.generate()
 }
